c26179675a03eefd29c56fb4b31029b5f43c50cf
alexeykaratay <karatayalexey@gmail.com>
2025-01-20 01:19:33 +0300

    Lesson10 added

---
+++ src/main/java/lesson10/Main.java
@@ -0,0 +1,27 @@
+package lesson10;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Box<Apple> box1 = new Box<>();
+        box1.add(new Apple());
+        box1.add(new Apple());
+        box1.add(new Apple());
+
+        Box<Orange> box2 = new Box<>();
+        box2.add(new Orange());
+        box2.add(new Orange());
+
+        System.out.println(box1.getWeight());
+        System.out.println(box2.getWeight());
+        System.out.println(box1.compare(box2));
+
+        Box<Apple> box3 = new Box<>();
+        box3.add(new Apple());
+        box3.add(new Apple());
+
+        box1.pourTo(box3);
+        System.out.println(box1.getWeight());
+        System.out.println(box3.getWeight());
+    }
+}
