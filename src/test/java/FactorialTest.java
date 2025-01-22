f19f7211c0221f5ad28ecbdc3c929b1826cb10e4
alexeykaratay <karatayalexey@gmail.com>
2025-01-22 22:28:08 +0300

    Lesson_12_testng

--- src/test/java/FactorialTest.java
+++ src/test/java/FactorialTest.java
@@ -1,44 +1,40 @@
-import org.junit.jupiter.api.Test;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
 
 import java.math.BigInteger;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class FactorialTest {
 
     @Test
     void testFactorialOfZero() {
-        assertEquals(BigInteger.ONE, Factorial.calculateFactorial(0));
+        Assert.assertEquals(BigInteger.ONE, Factorial.calculateFactorial(0));
     }
 
     @Test
     void testFactorialOfOne() {
-        assertEquals(BigInteger.ONE, Factorial.calculateFactorial(1));
+        Assert.assertEquals(BigInteger.ONE, Factorial.calculateFactorial(1));
     }
 
     @Test
     void testFactorialOfFive() {
-        assertEquals(BigInteger.valueOf(120), Factorial.calculateFactorial(5));
+        Assert.assertEquals(BigInteger.valueOf(120), Factorial.calculateFactorial(5));
     }
 
     @Test
     void testFactorialOfTen() {
-        assertEquals(BigInteger.valueOf(3628800), Factorial.calculateFactorial(10));
+        Assert.assertEquals(BigInteger.valueOf(3628800), Factorial.calculateFactorial(10));
     }
 
-    @Test
+    @Test(expectedExceptions = IllegalArgumentException.class,
+            expectedExceptionsMessageRegExp = "Факториал не может быть отрицательным")
     void testNegativeNumber() {
-        Exception exception = assertThrows(IllegalArgumentException.class,
-                () -> Factorial.calculateFactorial(-5));
-
-        assertEquals("Факториал не может быть отрицательным числом", exception.getMessage());
+        Factorial.calculateFactorial(-5);
     }
 
     @Test
     void testLargeFactorial() {
         BigInteger expected = new BigInteger("2432902008176640000"); // 20!
-        assertEquals(expected, Factorial.calculateFactorial(20));
+        Assert.assertEquals(expected, Factorial.calculateFactorial(20));
     }
 }
-
